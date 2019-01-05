6ccf6bf542b277096b918207bc87d4f514145f94
Lilia Hamouda <hamouda@stud.fra-uas.de>
2019-01-05 14:59:57 +0100

    Conflict solving

--- RealTimeScheduler/src/scheduler/Task.java
+++ RealTimeScheduler/src/scheduler/Task.java
@@ -1,46 +1,106 @@
 package scheduler;
 
 public class Task implements Comparable<Task>{
-	private int period;
-    private int execution;
-    private int remainingE;
-    private int entry;
-    private int id;
+	int period;
+	int execution;
+	int id;
+	int remainingE;
+	int deadline;
+	int count;
+	
+	public int getPeriod() {
+		return period;
+	}
 
-	public Task(int id, int period, int execution){
+	public void setPeriod(int period) {
+		this.period = period;
+	}
+
+	public int getExecution() {
+		return execution;
+	}
+
+	public void setExecution(int execution) {
+		this.execution = execution;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getRemainingE() {
+		return remainingE;
+	}
+
+	public void setRemainingE(int remainingE) {
+		this.remainingE = remainingE;
+	}
+
+	public int getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(int deadline) {
+		this.deadline = deadline;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public Task(int period,int execution,int id){
 		this.id = id;
 		this.period = period;
 		this.execution = execution;
-		this.remainingE = execution;
+		this.deadline= period;
+		this.remainingE= execution;
+		this.count=0;
 	}
 
-	public int compareTo(Task task) {
-
-		if (this.period >= task.period) {
-		    return 1;
-		} else {
-			return -1;
+	 public int compareTo(Task task) {
+         if(this.deadline>= task.deadline){
+        	 return 1;
+         }else{
+        	 return -1;
+         }
+     }
+	  
+	//A method to check if a task is executable.
+	 public boolean started(int position)
+		{
+			if(position>=(this.count*this.period))
+			{
+				return true;
+			}
+			return false;
 		}
-  	}
-
-    public int getPeriod() {
-        return period;
-    }
-
-    public int getExecution() {
-        return execution;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", period=" + period +
-                ", execution=" + execution +
-                '}';
-    }
+	 
+	//Increase count value after complete execution.
+	 public void increaseCount()
+	 {
+		 this.count++;
+	 }
+	 
+	//Reset the value of remainingE after task execution.
+	 public void resetExecution()
+	 {
+		this.remainingE=this.execution;
+	 }
+	 
+	    @Override
+	    public String toString() {
+	        return "Task{" +
+	                "id=" + id +
+	                ", period=" + period +
+	                ", execution=" + execution +
+	                '}';
+	    }
 }
